12c8c5fb88017d8f327ad641f62b8aacb420941f
Avishkar Pawar <avishkarpawar004@gmail.com>
2024-02-08 11:15:17 +0530

    feat: wire analytics button to bar graph view

--- expense_calculator/Expense_Calculator.java
+++ expense_calculator/Expense_Calculator.java
@@ -362,3 +362,4 @@ public class Expense_Calculator extends javax.swing.JFrame {
 // Full UI layout managed by NetBeans form editor
 // Add transaction button handler opens dialog frame
 // Delete confirmation shows selected transaction details
+// Analytics button launches graph window
