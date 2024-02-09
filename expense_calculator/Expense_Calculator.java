33eea3184ccc493f025b4359afee0d4a748e2cbc
Avishkar Pawar <avishkarpawar004@gmail.com>
2024-02-09 15:50:33 +0530

    feat: implement cancel button in delete dialog

--- expense_calculator/Expense_Calculator.java
+++ expense_calculator/Expense_Calculator.java
@@ -363,3 +363,4 @@ public class Expense_Calculator extends javax.swing.JFrame {
 // Add transaction button handler opens dialog frame
 // Delete confirmation shows selected transaction details
 // Analytics button launches graph window
+// Cancel button hides delete dialog
